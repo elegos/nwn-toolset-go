fdb6ed0b8915a95543d77f03b392913faa995ea1
Giacomo Furlan <giacomo.furlan@fastwebnet.it>
2017-12-24 17:38:36 +0100

    [cli] moved file flag to mandatory argument

--- src/cli/main.go
+++ src/cli/main.go
@@ -4,24 +4,42 @@ import (
 	"aurora/file/erf"
 	"cli/command"
 	"flag"
+	"fmt"
+	"os"
 )
 
-func extract(module *erf.ERF) {
-
-}
-
 func main() {
-	fileName := flag.String("file", "", "the module's file name")
-	extractFlag := flag.String("extract", "", "ACTION: extract the file contents in the specified directory")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stdout, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprintf(os.Stdout, "%s [flags] moduleFileName\n", os.Args[0])
+		fmt.Fprintln(os.Stdout, "")
+		fmt.Fprint(os.Stdout, "Flags:\n")
+		flag.PrintDefaults()
+		fmt.Fprint(os.Stdout, "\n")
+	}
 
+	extract := flag.String("extract", "", "ACTION: extract the file contents in the specified directory")
 	flag.Parse()
 
+	fileName := flag.Arg(0)
+
+	stat, err := os.Stat(fileName)
+
+	if err != nil || stat == nil || stat.IsDir() {
+		fmt.Println(fmt.Sprintf("%s: directory or not existing file", fileName))
+		fmt.Println("")
+		flag.Usage()
+
+		return
+	}
+
 	// Some stats
-	module := erf.FromFile(*fileName)
+	module := erf.FromFile(fileName)
 
 	command.DescribeErf(&module)
 
-	if *extractFlag != "" {
-		command.ExtractErf(&module, *extractFlag)
+	if *extract != "" {
+		command.ExtractErf(&module, *extract)
 	}
 }
