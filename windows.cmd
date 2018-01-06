78e0dc12bcd22d2d29e92e2ecde78dccd408fd8f
Giacomo Furlan <giacomo.furlan@fastwebnet.it>
2018-01-07 00:54:42 +0100

    Added tests to windows command

--- windows.cmd
+++ windows.cmd
@@ -4,12 +4,21 @@ shift
 
 if "%arg%" == "/?" (
   echo "/c clean binaries"
+  echo "/t run tests"
   echo "/a clean and rebuild binaries"
   exit /b
 )
 
 if "%arg%" == "/c" (
   ninja -f windows.ninja -t clean
+  exit /b
+)
+
+if "%arg%" == "/t" (
+  set "GOPATH=%cd%"
+  set "GOBIN=%cd%\bin"
+
+  go test -v -cover aurora/...
   exit /b
 )
 
