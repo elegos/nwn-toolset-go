78e6e521b2cde92433d6e645b78ebbf494de281a
Giacomo Furlan <giacomo.furlan@fastwebnet.it>
2018-01-15 02:58:04 +0100

    Travis / Codecov support

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
 
+set -e
+
 export GOPATH=`pwd`
 export GOBIN=`pwd`/bin
+export GOMAXPROCS=`grep -c ^processor /proc/cpuinfo`
+
+# Remove old coverage file
+rm -f coverage.txt
+rm -f coverage.*.txt
 
 # Aurora library
-go test -v -cover aurora/...
+declare -A PACKAGES=(
+  ["aurora.file.erf"]="aurora/file/erf"
+  ["aurora.file.gff"]="aurora/file/gff"
+
+  ["aurora.tools"]="aurora/tools"
+  ["aurora.tools.fileReader"]="aurora/tools/fileReader"
+)
+
+# Download all the required packages for the aurora library
+go get -t -v aurora/...
+# Execute the tests
+for fileName in ${!PACKAGES[@]}; do
+  go test -race \
+    -cover \
+    -covermode=atomic \
+    -coverprofile=coverage.${fileName}.txt \
+    ${PACKAGES[$fileName]}
+done
+
+sed -i '/mode: atomic/d' coverage.*.txt
+echo "mode: atomic" > coverage.txt
+cat coverage.*.txt >> coverage.txt
+rm -f coverage.*.txt
